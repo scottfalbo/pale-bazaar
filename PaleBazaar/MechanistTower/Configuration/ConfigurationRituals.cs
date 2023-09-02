34ef73fcae8a605ce7877b32a0a1f6285dfdbeef
Scott Falbo <scottfalboart@gmail.com>
2023-09-02 15:15:15 -0700

    abstracted app settings to config class

---
+++ PaleBazaar/MechanistTower/Configuration/ConfigurationRituals.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using PaleBazaar.GuardianAegis;
+
+namespace PaleBazaar.MechanistTower.Configuration
+{
+    public static class ConfigurationRituals
+    {
+        public static void ImbueConstruct(WebApplication app)
+        {
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+                DbInitializer.InitializeAsync(userManager, roleManager).Wait();
+            }
+
+            app.MapControllers();
+            app.MapBlazorHub();
+            app.MapFallbackToPage("/_Host");
+        }
+    }
+}
--- PaleBazaar/Program.cs
+++ PaleBazaar/Program.cs
@@ -43,37 +43,6 @@ builder.Configuration.AddAzureKeyVault(new Uri(keyVaultUri), new ClientSecretCre
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
-}
-else
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-using (var scope = app.Services.CreateScope())
-{
-    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
-    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-
-    DbInitializer.InitializeAsync(userManager, roleManager).Wait();
-}
-
-app.MapControllers();
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
+ConfigurationRituals.ImbueConstruct(app);
 
 app.Run();
