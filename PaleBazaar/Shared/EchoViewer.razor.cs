43f3c5ca35923ac077d82dc17c4b762e178f0e98
Scott Falbo <scottfalboart@gmail.com>
2023-09-08 20:19:02 -0700

    moved echo viewer logic to a code behind class

---
+++ PaleBazaar/Shared/EchoViewer.razor.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Components;
+using PaleBazaar.MechanistTower.Entities;
+using PaleBazaar.MechanistTower.SpellChanters;
+
+namespace PaleBazaar.Shared
+{
+    public partial class EchoViewer : ComponentBase
+    {
+        [Inject]
+        public IEchoChanters EchoChanters { get; set; }
+
+        [Parameter] public List<Echo> Echoes { get; set; }
+        [Parameter] public int ActiveEchoIndex { get; set; }
+        [Parameter] public EventCallback<int> OnEchoEvocation { get; set; }
+        [Parameter] public bool IsWizardOverLord { get; set; }
+
+        private Echo EchoToShatter;
+        private bool IsCarouselVisible = false;
+
+        private string GetCarouselClasses()
+        {
+            return IsCarouselVisible ? "" : "hide-me";
+        }
+
+        private void ShowCarousel(int index)
+        {
+            ActiveEchoIndex = index;
+            IsCarouselVisible = true;
+        }
+
+        private void HideCarousel()
+        {
+            IsCarouselVisible = false;
+        }
+
+        private async Task ShatterEcho()
+        {
+            await EchoChanters.ShatterEcho(
+                EchoToShatter.Id,
+                EchoToShatter.PartitionKey,
+                EchoToShatter.FileName,
+                EchoToShatter.ThumbnailFileName);
+        }
+    }
+}
