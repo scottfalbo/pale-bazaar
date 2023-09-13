37f4ea88cf2fd30e6c71f140d9642460645a7c0b
Scott Falbo <scottfalboart@gmail.com>
2023-09-12 22:55:30 -0700

    simplified mapper logic

--- PaleBazaar/MechanistTower/Transmutators/EchoTransmutator.cs
+++ PaleBazaar/MechanistTower/Transmutators/EchoTransmutator.cs
@@ -6,27 +6,7 @@ namespace PaleBazaar.MechanistTower.Transmutators
     [Mapper]
     public partial class EchoTransmutator
     {
-        public Echo InfernalContractToEcho(InfernalContract infernalContract)
-        {
-            return new Echo()
-            {
-                Id = infernalContract.Id,
-                PartitionKey = infernalContract.PartitionKey,
-                EternalSymbol = infernalContract.EternalSymbol,
-                CreatedDateTime = infernalContract.CreatedDateTime,
-                Name = infernalContract.Name,
-                ImageUrl = infernalContract.ImageUrl,
-                ThumbnailUrl = infernalContract.ThumbnailUrl,
-                Display = infernalContract.Display,
-                AltText = infernalContract.AltText,
-                FileName = infernalContract.FileName,
-                ThumbnailFileName = infernalContract.ThumbnailFileName,
-                Medium = infernalContract.Medium,
-                Size = infernalContract.Size,
-                IsForSale = infernalContract.IsForSale,
-                Price = infernalContract.Price
-            };
-        }
+        public partial Echo InfernalContractToEcho(InfernalContract infernalContract);
 
         public partial InfernalContract EchoToInfernalContract(Echo echo);
     }
