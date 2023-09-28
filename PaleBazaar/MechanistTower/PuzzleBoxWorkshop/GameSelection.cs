8afd993a55a27cdd6a6c35d3d63f04e25424529c
Scott Falbo <scottfalboart@gmail.com>
2023-09-27 22:20:48 -0700

    whatever

---
+++ PaleBazaar/MechanistTower/PuzzleBoxWorkshop/GameSelection.cs
@@ -0,0 +1,9 @@
+﻿namespace PaleBazaar.MechanistTower.PuzzleBoxWorkshop
+{
+    public static class GameSelection
+    {
+        public const string CipherBox = nameof(CipherBox);
+
+        public const string Welcome = nameof(Welcome);
+    }
+}
--- PaleBazaar/Pages/Grimoires/PuzzleBox.razor.cs
+++ PaleBazaar/Pages/Grimoires/PuzzleBox.razor.cs
@@ -1,6 +1,32 @@
-﻿namespace PaleBazaar.Pages.Grimoires
+﻿using PaleBazaar.MechanistTower.PuzzleBoxWorkshop;
+using System;
+using Microsoft.AspNetCore.Components;
+
+namespace PaleBazaar.Pages.Grimoires
 {
     public partial class PuzzleBox
     {
+        private Type _gameType;
+        protected string Game { get; set; }
+
+        // Called when the page is first loaded
+        protected override void OnInitialized()
+        {
+            Game = "Welcome";
+            UpdateGameType();
+        }
+
+        // Called when the drop-down menu value changes
+        protected void UpdateGame(ChangeEventArgs e)
+        {
+            Game = e.Value.ToString();
+            UpdateGameType();
+        }
+
+        // Updates the _gameType based on the Game variable
+        private void UpdateGameType()
+        {
+            _gameType = Type.GetType($"PaleBazaar.Pages.Grimoires.PuzzleBoxGames.{Game}, PaleBazaar.dll");
+        }
     }
 }
