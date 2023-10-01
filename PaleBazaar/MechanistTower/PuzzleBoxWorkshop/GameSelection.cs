f29dd3f0ddf282214431334820fa861f543faef9
Scott Falbo <scottfalboart@gmail.com>
2023-10-01 14:27:53 -0700

    refactoried puxxlebox to content menu

--- PaleBazaar/MechanistTower/PuzzleBoxWorkshop/GameSelection.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PaleBazaar.MechanistTower.PuzzleBoxWorkshop
-{
-    public static class GameSelection
-    {
-        public const string CipherBox = nameof(CipherBox);
-
-        public const string Welcome = nameof(Welcome);
-    }
-}
