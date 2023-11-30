c660d3fe3373043b2d3a2689dbb70692fcdd5da4
Scott Falbo <scottfalboart@gmail.com>
2023-11-29 17:59:12 -0800

    added signal r hub

---
+++ PaleBazaar/MechanistTower/Beacons/ChatBeacon.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace PaleBazaar.MechanistTower.Beacons
+{
+    public class ChatBeacon : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
